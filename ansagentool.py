513d0e7e35a2a52cf1bde005da8e7cdec296a564
NekoNina <nina@nekonina.de>
2025-05-11 17:26:57 +0200

    added canceled, added platform change

--- ansagentool.py
+++ ansagentool.py
@@ -13,7 +13,7 @@ tableCities = pandas.read_excel(basePath + "_info/2013_01_02_info.xls", sheet_na
 tableReasons = pandas.read_excel(basePath + "_info/2013_01_02_info.xls", sheet_name="grund_dafuer ") #Path to the Info Excel file
 
 # API Request
-URL = "https://dbf.finalrewind.org/Karlsruhe%20Hbf.json?detailed=1&version=3"
+URL = "https://dbf.finalrewind.org/Mannheim%20Hbf.json?detailed=1&version=3"
 apiResponse = requests.get(url=URL,stream=True).json()
 
 platformAudio = basePath + f"dt/module_3_1/016.wav"
@@ -29,17 +29,22 @@ attentionArrival = basePath + f"dt/module_3_1/046.wav"
 gongdefault = basePath + f"gong/513/513_1.wav"
 doNotBoard = basePath + f"dt/module_3_1/007.wav"
 trainInformationOn = basePath + f"dt/module_3_1/030.wav"
+trainCancelledAudio = basePath + f"dt/module_3_1/014.wav"
+trainTrackChangeAudio = basePath + f"dt/module_3_1/023.wav"
+iRepeatAudio = basePath + f"dt/module_3_1/025.wav"
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 #Generating Numbers
-def speakNumber(number):
+def speakNumber(number,hoch=True):
     print(number)
+    heightString1 = "hoch"
+    if not hoch: heightString1 = "tief"
     numberAudioRequest = int(re.findall(r'\d+',number)[0])
 #If num between 0 and 100
     if numberAudioRequest <= 100 or numberAudioRequest == 200 or numberAudioRequest == 300 or numberAudioRequest == 400 or numberAudioRequest == 500 or numberAudioRequest == 600 or numberAudioRequest == 700 or numberAudioRequest == 800 or numberAudioRequest == 900:
-        numberAudio = basePath + f"dt/gleise_zahlen/hoch/{numberAudioRequest}.wav"
+        numberAudio = basePath + f"dt/gleise_zahlen/{heightString1}/{numberAudioRequest}.wav"
         playsound(numberAudio)
     #if num between 101 and 999
     elif numberAudioRequest <=999 and not numberAudioRequest == 100 and not numberAudioRequest == 200 and not numberAudioRequest == 300 and not numberAudioRequest == 400 and not numberAudioRequest == 500 and not numberAudioRequest == 600 and not numberAudioRequest == 700 and not numberAudioRequest == 800 and not numberAudioRequest == 900:
@@ -49,9 +54,9 @@ def speakNumber(number):
         if str(numberAudioRequest)[1] == "0":
             hunLastTwoDigits = str(numberAudioRequest)[2:]
             print(hunLastTwoDigits)
-            hunLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/hoch/{hunLastTwoDigits}.wav"
+            hunLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/{heightString1}/{hunLastTwoDigits}.wav"
         else:
-            hunLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/hoch/{hunLastTwoDigits}.wav"
+            hunLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/{heightString1}/{hunLastTwoDigits}.wav"
         playsound(hundretsAudio)
         playsound(hunLastTwoDigitsAudio)
 
@@ -60,17 +65,17 @@ def speakNumber(number):
         if str(numberAudioRequest)[2:] == "00":
             thoFirstTwoDigits = str(numberAudioRequest)[:2]
             thoFirstTwoDigitsAudio = basePath + f"dt/gleise_zahlen/tief/{thoFirstTwoDigits}.wav"
-            thoLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/hoch/00.wav"
+            thoLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/{heightString1}/00.wav"
         if str(numberAudioRequest)[2] == "0":
             thoFirstTwoDigits = str(numberAudioRequest)[:2]
             thoFirstTwoDigitsAudio = basePath + f"dt/gleise_zahlen/tief/{thoFirstTwoDigits}.wav"
             thoLastTwoDigits = str(numberAudioRequest)[3]
-            thoLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/hoch/0{thoLastTwoDigits}.wav"
+            thoLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/{heightString1}/0{thoLastTwoDigits}.wav"
         else:
             thoFirstTwoDigits = str(numberAudioRequest)[:2]
             thoFirstTwoDigitsAudio = basePath + f"dt/gleise_zahlen/tief/{thoFirstTwoDigits}.wav"
             thoLastTwoDigits = str(numberAudioRequest)[2:]
-            thoLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/hoch/{thoLastTwoDigits}.wav"
+            thoLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/{heightString1}/{thoLastTwoDigits}.wav"
         playsound(thoFirstTwoDigitsAudio)
         playsound(thoLastTwoDigitsAudio)
     #if num 5 digits
@@ -80,21 +85,21 @@ def speakNumber(number):
             centFirstTwoDigitsAudio = basePath + f"dt/gleise_zahlen/tief/{centFirstTwoDigits}.wav"
             centMiddleDigitAudio = basePath + f"dt/gleise_zahlen/tief/0.wav"
             centLastTwoDigits = str(numberAudioRequest)[3:]
-            centLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/hoch/{centLastTwoDigits}.wav"
+            centLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/{heightString1}/{centLastTwoDigits}.wav"
         if str(numberAudioRequest)[3:] == "00":
             centFirstTwoDigits = str(numberAudioRequest)[:2]
             centFirstTwoDigitsAudio = basePath + f"dt/gleise_zahlen/tief/{centFirstTwoDigits}.wav"
             centMiddleDigit = str(numberAudioRequest)[3]
             centMiddleDigitAudio = basePath + f"dt/gleise_zahlen/tief/{centMiddleDigit}.wav"
             centLastTwoDigits = str(numberAudioRequest)[4]
-            centLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/hoch/0{centLastTwoDigits}.wav"
+            centLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/{heightString1}/0{centLastTwoDigits}.wav"
         else:
             centFirstTwoDigits = str(numberAudioRequest)[:2]
             centFirstTwoDigitsAudio = basePath + f"dt/gleise_zahlen/tief/{centFirstTwoDigits}.wav"
             centMiddleDigit = str(numberAudioRequest)[2]
             centMiddleDigitAudio = basePath + f"dt/gleise_zahlen/tief/{centMiddleDigit}.wav"
             centLastTwoDigits = str(numberAudioRequest)[3:]
-            centLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/hoch/{centLastTwoDigits}.wav"
+            centLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/{heightString1}/{centLastTwoDigits}.wav"
         playsound(centFirstTwoDigitsAudio)
         playsound(centMiddleDigitAudio)
         playsound(centLastTwoDigitsAudio)
@@ -105,20 +110,20 @@ def speakNumber(number):
             milFirstTwoDigitsAudio = basePath + f"dt/gleise_zahlen/tief/{milFirstTwoDigits}.wav"
             milMiddleTwoDigitsAudio = basePath + f"dt/gleise_zahlen/tief/00.wav"
             milLastTwoDigits = str(numberAudioRequest)[4:]
-            milLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/hoch/{milLastTwoDigits}.wav"
+            milLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/{heightString1}/{milLastTwoDigits}.wav"
         if str(numberAudioRequest)[4:] == "00":
             milFirstTwoDigits = str(numberAudioRequest)[:2]
             milFirstTwoDigitsAudio = basePath + f"dt/gleise_zahlen/tief/{milFirstTwoDigits}.wav"
             milMiddleTwoDigits = str(numberAudioRequest)[2:4]
             milMiddleTwoDigitsAudio = basePath + f"dt/gleise_zahlen/tief/{milMiddleTwoDigits}.wav"
-            milLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/hoch/00.wav"
+            milLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/{heightString1}/00.wav"
         else:
             milFirstTwoDigits = str(numberAudioRequest)[:2]
             milFirstTwoDigitsAudio = basePath + f"dt/gleise_zahlen/tief/{milFirstTwoDigits}.wav"
             milMiddleTwoDigits = str(numberAudioRequest)[2:4]
             milMiddleTwoDigitsAudio = basePath + f"dt/gleise_zahlen/tief/{milMiddleTwoDigits}.wav"
             milLastTwoDigits = str(numberAudioRequest)[4:]
-            milLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/hoch/{milLastTwoDigits}.wav"
+            milLastTwoDigitsAudio = basePath + f"dt/gleise_zahlen/{heightString1}/{milLastTwoDigits}.wav"
         playsound(milFirstTwoDigitsAudio)
         playsound(milMiddleTwoDigitsAudio)
         playsound(milLastTwoDigitsAudio)
@@ -168,27 +173,7 @@ def speakDelayReason(reasonKeyword):
     reasonAudio = basePath + f"/dt/gruende/grund_dafuer/{sortedReason.iloc[0]["Datei-Name"]}"
     playsound(reasonAudio)
 
-# Definition of the announcement elements
-for departure in apiResponse["departures"]:
-    trainPlatform = departure["platform"]
-    trainFriendlyName = departure["train"]
-    trainDestination = departure["destination"]
-    try:
-        trainOrigin = departure["route"][0]["name"]
-    except:
-        pass
-    trainVia = departure["via"]
-    trainScheduledArrival = departure["scheduledArrival"]
-    trainScheduluedDeparture = departure["scheduledDeparture"]
-    trainArrivalDelay = departure["delayArrival"]
-    trainDepartureDelay = departure["delayDeparture"]
-    trainIsCancelled = departure["isCancelled"]
-    trainDelayMessage = departure["messages"]["delay"]
-
-  
-
-    if trainPlatform == "":
-        continue
+def arrivalOfTrain():
     playsound(gongdefault)
     playsound(platformAudio)
     speakNumber(trainPlatform)
@@ -221,6 +206,8 @@ for departure in apiResponse["departures"]:
         playsound(doNotBoard)
     playsound(attentionArrival)
 
+
+def delayInformation():
     if (trainDepartureDelay or 0) >=5:
         playsound(trainInformationOn)
         speakTrainName(trainFriendlyName)
@@ -249,5 +236,91 @@ for departure in apiResponse["departures"]:
         playsound(basePath + f"dt/zeiten/verspaetung_heute/{h:03}.wav")
     #if (trainDepartureDelay or 0) >= 10 or (trainArrivalDelay or 0) >=10:
     #    speakDelayReason(trainDelayMessage)
+
+def cancelInformaton():
+    if trainIsCancelled == 1:
+        playsound(gongdefault)
+        playsound(trainInformationOn)
+        speakTrainName(trainFriendlyName)
+        if not trainScheduluedDeparture == None or trainScheduluedDeparture == "":
+            playsound(trainToAudio)
+            speakCity(trainDestination)
+            playsound(departureOnTimeAudio)
+            speakTime(trainScheduluedDeparture)
+        else:
+            playsound(trainFromAudio)
+            speakCity(trainOrigin)
+            playsound(arrivalOnTimeAudio)
+            speakTime(trainScheduledArrival)
+        playsound(trainCancelledAudio)
+
+def trackChangeInformation():
+    if trainScheduledPlatform != trainPlatform:
+        playsound(gongdefault)
+        playsound(trainInformationOn)
+        speakTrainName(trainFriendlyName)
+        if not trainScheduluedDeparture == None or trainScheduluedDeparture == "":
+            playsound(trainToAudio)
+            speakCity(trainDestination)
+            playsound(departureOnTimeAudio)
+            speakTime(trainScheduluedDeparture)
+        else:
+            playsound(trainFromAudio)
+            speakCity(trainOrigin)
+            playsound(arrivalOnTimeAudio)
+            speakTime(trainScheduledArrival)
+        playsound(trainTrackChangeAudio)
+        speakNumber(trainPlatform, False)
+        playsound(iRepeatAudio)
+        if trainScheduledPlatform != trainPlatform:
+            playsound(trainInformationOn)
+            speakTrainName(trainFriendlyName)
+            if not trainScheduluedDeparture == None or trainScheduluedDeparture == "":
+                playsound(trainToAudio)
+                speakCity(trainDestination)
+                playsound(departureOnTimeAudio)
+                speakTime(trainScheduluedDeparture)
+            else:
+                playsound(trainFromAudio)
+                speakCity(trainOrigin)
+                playsound(arrivalOnTimeAudio)
+                speakTime(trainScheduledArrival)
+        playsound(trainTrackChangeAudio)
+        speakNumber(trainPlatform, False)
+
+
         
+
+# Definition of the announcement elements
+for departure in apiResponse["departures"]:
+    trainPlatform = departure["platform"]
+    try:
+        trainScheduledPlatform = departure["scheduledPlatform"]
+    except:
+        pass
+    trainFriendlyName = departure["train"]
+    trainDestination = departure["destination"]
+    try:
+        trainOrigin = departure["route"][0]["name"]
+    except:
+        pass
+    trainVia = departure["via"]
+    trainScheduledArrival = departure["scheduledArrival"]
+    trainScheduluedDeparture = departure["scheduledDeparture"]
+    trainArrivalDelay = departure["delayArrival"]
+    trainDepartureDelay = departure["delayDeparture"]
+    trainIsCancelled = departure["isCancelled"]
+    trainDelayMessage = departure["messages"]["delay"]
+
+  
+
+    if trainPlatform == "":
+        continue
+    
+    #arrivalOfTrain()
+    #delayInformation()
+    #cancelInformaton()
+    print(trainPlatform + " " + trainScheduledPlatform)
+    trackChangeInformation()
+
     #input()
